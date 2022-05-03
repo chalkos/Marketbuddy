a19f76d60ce9c39612c06e96cce21adc322c7d00
Bruno Ferreira <chalkos@chalkos.net>
2022-05-04 00:07:58 +0100

    fixes #16 by handling clipboard exceptions

---
+++ Marketbuddy/Common/ImGuiEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ImGuiNET;
+
+namespace Marketbuddy.Common;
+
+public static class ImGuiEx
+{
+    internal static string GetClipboardText()
+    {
+        try
+        {
+            return ImGui.GetClipboardText() ?? string.Empty;
+        }
+        catch (Exception)
+        {
+            return string.Empty;
+        }
+    }
+}
